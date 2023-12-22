7201e0dc971066b9cfbc433b0f9037b412e00a7e
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-12-22 15:21:22 -0300

    FIX: some logic programming issues

--- src/scripts/fmrDeposit.js
+++ src/scripts/fmrDeposit.js
@@ -1,21 +1,20 @@
 // Process the deposit values inserted by the user
-const frmDeposit = (depositFmr, depositIn, depositOut, depositModal) => {
+const frmDeposit = (depositFmr, depositIn, depositModal) => {
 
     depositFmr.addEventListener("submit", (e) => {      // Listen the deposit's form submit
 
-        e.preventDefault();     // Prevent the form's automatic behaviour
+        e.preventDefault();     // Prevents the form's automatic behaviour
 
         const lastDeposit = Number(localStorage.getItem("lastDeposit"));    // Get the last deposit
         let depositSum = Number(localStorage.getItem("depositSum"));        // Get the sum of the deposits
         depositSum += lastDeposit;                                          // Sum the deposits
         localStorage.setItem("depositSum", depositSum);                     // Save the sum of the the deposits
 
-        const depositValue = Number(depositIn.value);   // Get the deposit value
+        const depositValue = Number(depositIn.value);   // Get the deposit's value
 
         localStorage.setItem("lastDeposit", depositValue);      // Save the deposit
 
-        depositOut.innerHTML = depositValue;    // Write the last deposit
-
+        depositIn.value = "";       // Clean the input
         depositModal.close();       // Close the modal after the submit event        
 
     });
